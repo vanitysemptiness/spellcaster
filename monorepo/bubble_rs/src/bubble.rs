b82eea2eadc5250fde0f30ea65cf00cb68578d1c
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2024-02-18 19:53:11 -0500

    bubble base written in Rust

--- monorepo/bubble_rs/src/bubble.rs
+++ monorepo/bubble_rs/src/bubble.rs
@@ -1,5 +1,93 @@
 
+#[warn(dead_code)]
+mod bubble {
 
-struct Bubble {
-    text: String,
+    const TOPIC_SEPARATOR: &str = "::";
+
+    struct Bubble {
+        text: String,
+        topics: Vec<String>
+    }
+
+    pub struct DirtyBubble {
+        raw: String,
+    }
+
+    #[allow(private_interfaces)]
+    impl DirtyBubble {
+        // Constructor method to create a new DirtyBubble instance
+        pub fn new(raw: &str) -> DirtyBubble {
+            DirtyBubble { raw: String::from(raw) }
+        }
+
+        // Clean method that splits the input string on "::" and trims whitespace
+        pub fn clean(&self) -> Bubble {
+            let mut topics: Vec<String> = self.raw
+                .split(TOPIC_SEPARATOR)
+                .map(|s| s.trim().to_string())
+                .collect();
+            let text: String = topics.pop().unwrap_or_default();
+            let bubble: Bubble = Bubble::new(&text, topics);
+            bubble
+        }
+    }
+
+    impl Bubble {
+        fn new(text: &str, topics: Vec<String>) -> Bubble {
+            Bubble {
+                text: text.to_string(),
+                topics,
+            }
+        }
+    }
+
+    // unit tests
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_create_bubble() {
+            // Arrange
+            let text = "This is a bubble";
+            let topics = vec![
+                "Rust".to_string(),
+                "Testing".to_string(),
+            ];
+
+            // Act
+            let bubble = Bubble::new(&text, topics.clone());
+
+            // Assert
+            assert_eq!(bubble.text, text);
+            assert_eq!(bubble.topics, topics);
+        }
+
+        #[test]
+        fn create_dirty_bubble() {
+            let text: String = String::from("quote:: those who know do not speak. those who speak do not know -lau tsu");
+            let dirty: DirtyBubble = DirtyBubble::new(&text);
+            let bubble: Bubble = dirty.clean();
+            let expected_text: String = String::from("those who know do not speak. those who speak do not know -lau tsu");
+            assert_eq!(expected_text, bubble.text);
+            assert_eq!(1, bubble.topics.len());
+            assert_eq!(String::from("quote"), bubble.topics[0])
+        }
+
+        #[test]
+        fn create_dirty_bubble_multiple_topics() {
+            let text: String = String::from("reflection:: philosopy:: quote:: those who know do not speak. those who speak do not know -lau tsu");
+            let dirty: DirtyBubble = DirtyBubble::new(&text);
+            let bubble: Bubble = dirty.clean();
+            let expected_text: String = String::from("those who know do not speak. those who speak do not know -lau tsu");
+            assert_eq!(expected_text, bubble.text);
+            assert_eq!(3, bubble.topics.len());
+            assert_eq!(String::from("reflection"), bubble.topics[0]);
+            assert_eq!(String::from("philosopy"), bubble.topics[1]);
+            assert_eq!(String::from("quote"), bubble.topics[2]);
+        }
+    }
 }
+
+
+
