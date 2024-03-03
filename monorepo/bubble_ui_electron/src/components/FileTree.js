0e6b9588afca9d16915f053e75723456eebba28a
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2024-03-03 00:26:25 -0500

    added in an electron ui project, using quill for note taking features

---
+++ monorepo/bubble_ui_electron/src/components/FileTree.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FileTree = () => {
+  // Your file tree logic goes here
+  return (
+    <div>
+      {/* Your file tree content goes here */}
+    </div>
+  );
+};
+
+export default FileTree;
